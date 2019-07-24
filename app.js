7cebe12849390fdbe708fc80173b269514f78920
Luc Merceron <luc@procsea.com>
2019-07-24 15:29:05 +0200

    Some mods

--- app.js
+++ app.js
@@ -2,48 +2,41 @@
  * Created by acunningham on 07/04/17.
  */
 
-'use strict';
+"use strict";
 
-const Keycloak = require('keycloak-connect');
-const express = require('express');
-const session = require('express-session');
-const expressHbs = require('express-handlebars');
+const Keycloak = require("keycloak-connect");
+const express = require("express");
+const session = require("express-session");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-
 // Register 'handelbars' extension with The Mustache Express
-app.engine('hbs', expressHbs({extname:'hbs',
-  defaultLayout:'layout.hbs',
-  relativeTo: __dirname}));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  expressHbs({
+    extname: "hbs",
+    defaultLayout: "layout.hbs",
+    relativeTo: __dirname
+  })
+);
+app.set("view engine", "hbs");
 
+app.keycloak = new Keycloak({});
 
-var memoryStore = new session.MemoryStore();
-var keycloak = new Keycloak({ store: memoryStore });
-
-//session
-app.use(session({
-  secret:'thisShouldBeLongAndSecret',
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore
-}));
-
-app.use(keycloak.middleware());
+app.use(app.keycloak.middleware());
+app.use(app.keycloak.middleware({ logout: "/" }));
 
 //route protected with Keycloak
-app.get('/test', keycloak.protect(), function(req, res){
-  res.render('test', {title:'Test of the test'});
+app.get("/test", app.keycloak.protect(), function(req, res) {
+  res.render("test", { title: "Test of the test" });
 });
 
 //unprotected route
-app.get('/',function(req,res){
-  res.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.use( keycloak.middleware( { logout: '/'} ));
-
-app.listen(8000, function () {
-  console.log('Listening at http://localhost:8000');
+app.listen(3000, function() {
+  console.log("Listening at http://localhost:3000");
 });
