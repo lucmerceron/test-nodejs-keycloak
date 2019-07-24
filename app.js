d626776f7ae3ed2369e449984ac0bf434f28a3d7
François-Guillaume RIBREAU <github@fgribreau.com>
2019-07-24 16:13:08 +0200

    WiP

--- app.js
+++ app.js
@@ -13,30 +13,46 @@ const app = express();
 
 // Register 'handelbars' extension with The Mustache Express
 app.engine(
-  "hbs",
-  expressHbs({
-    extname: "hbs",
-    defaultLayout: "layout.hbs",
-    relativeTo: __dirname
-  })
+    "hbs",
+    expressHbs({
+        extname: "hbs",
+        defaultLayout: "layout.hbs",
+        relativeTo: __dirname
+    })
 );
 app.set("view engine", "hbs");
 
-app.keycloak = new Keycloak({});
 
-app.use(app.keycloak.middleware());
-app.use(app.keycloak.middleware({ logout: "/" }));
+
+const memoryStore = new session.MemoryStore();
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+
+
+app.keycloak = new Keycloak({
+    store: memoryStore
+});
+
+app.use(app.keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+}));
 
 //route protected with Keycloak
-app.get("/test", app.keycloak.protect(), function(req, res) {
-  res.render("test", { title: "Test of the test" });
+app.get("/test", app.keycloak.protect(), function (req, res) {
+    res.send('ok');
 });
 
 //unprotected route
-app.get("/", function(req, res) {
-  res.render("index");
+app.get("/", function (req, res) {
+    res.render("index");
 });
 
-app.listen(3000, function() {
-  console.log("Listening at http://localhost:3000");
+app.listen(3000, function () {
+    console.log("Listening at http://localhost:3000");
 });
